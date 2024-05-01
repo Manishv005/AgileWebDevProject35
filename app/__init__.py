73172832467e181c83e1728955b59cfe8430169b
nickthorton <nishandevkarnd@gmail.com>
2024-05-02 04:05:17 +0800

    Database model creation - Basic Structure of the Database model.py - Creates a fresh migration

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ flask_app.config.from_object(Config)
 # The database is represented in the app by an instance
 db = SQLAlchemy(flask_app)
 # The database migration engine also has an instance
-migrate = Migrate(flask_app, db)
+migrate = Migrate(flask_app, db,render_as_batch=True)
 # The login manager for the app using the flask_login package
 login = LoginManager(flask_app)
 login.login_view = 'login'
--- app/models.py
+++ app/models.py
@@ -26,15 +26,15 @@ def load_user(id):
 
 class User(UserMixin, db.Model):
     # Define the attributes
-    user_id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     # WriteOnlyMapped defines a collection meaning a one to many relationship
     # Define the relations
-    puzzles: so.WriteOnlyMapped["Puzzle"] = db.relationship(back_populates="creator", lazy="dynamic")
-    plays_games: so.WriteOnlyMapped["GameResult"] = db.relationship(back_populates="player", lazy="dynamic")
+    puzzles: so.WriteOnlyMapped["Puzzle"] = so.relationship(back_populates="creator", lazy="dynamic")
+    plays_games: so.WriteOnlyMapped["GameResult"] = so.relationship(back_populates="solver", lazy="dynamic")
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -45,31 +45,6 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-# Puzzle table creation
-class Puzzle(db.Model):
-    puzzle_id = db.Column(db.Integer, primary_key=True)
-    # word_id = db.Column(db.String(128), db.ForeignKey("Word.word_id"))
-    puzzle_creator: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.user_id))
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    creator: so.Mapped["User"] = db.relationship(back_populates="puzzles", lazy="dynamic")
-
-    def __repr__(self):
-        return f"<Puzzle {self.category}>"
-
-
-# Results Table Creation
-class GameResult(db.Model):
-    puzzle_id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(128))
-    username = db.Column(db.String(64))
-    time_spent = db.Column(db.Integer)
-    score = db.Column(db.Integer)
-
-    player: so.Mapped["User"] = db.relationship(back_populates="plays_games", lazy="dynamic")
-    def __repr__(self):
-        return f"<LeaderBoard {self.word} - {self.username}>"
-
 # Word Table Creation
 class Word(db.Model):
     word_id = db.Column(db.Integer, primary_key=True)
@@ -77,5 +52,36 @@ class Word(db.Model):
     word_length = db.Column(db.Integer)
     word = db.Column(db.String(128))
 
+    puzzle_word: so.Mapped["Puzzle"] = so.relationship(back_populates="has_word")
     def __repr__(self):
         return f"<Word {self.word}>"
+    
+# Puzzle table creation
+class Puzzle(db.Model):
+    puzzle_id = db.Column(db.Integer, primary_key=True)
+    word_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Word.word_id),
+                                               index=True)
+    puzzle_creator: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),index=True)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    creator: so.Mapped["User"] = so.relationship(back_populates="puzzles")
+    has_word: so.Mapped["Word"] = so.relationship(back_populates="puzzle_word")
+    puzzle_result: so.Mapped["GameResult"] = so.relationship(back_populates="has_puzzle")
+    def __repr__(self):
+        return f"<Puzzle {self.category}>"
+
+
+# Results Table Creation
+class GameResult(db.Model):
+    result_id = db.Column(db.Integer, primary_key=True)
+    puzzle_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Puzzle.puzzle_id),index=True)
+    puzzle_solver: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),index=True)
+    time_spent = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+    solver: so.Mapped["User"] = so.relationship(back_populates="plays_games")
+    has_puzzle: so.Mapped["Puzzle"] = so.relationship(back_populates="puzzle_result")
+
+    def __repr__(self):
+        return f"<GameResult {self.word} - {self.username}>"
+
