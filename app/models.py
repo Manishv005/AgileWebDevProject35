934c7ec2a44b39747f11cd408006aebdf3c1a784
sandeep.srmcse@gmail.com <sandeep.srmcse@gmail.com>
2024-05-01 20:05:16 +0800

    LeaderBoard table creation.

--- app/models.py
+++ app/models.py
@@ -12,19 +12,35 @@ from app import db
 # These classes are mapped to actual rows in the database by the ORM engine
 # Here, we create a User class, representing a User table in the database
 
+
 class User(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256)) # Optional allows nulls
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(
+        sa.String(256)
+    )  # Optional allows nulls
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
 
     # Useful for debugging only, tells python how to print the objects of this class
     def __repr__(self):
-        return '<User {}>'.format(self.username)
-    
-''' 
+        return "<User {}>".format(self.username)
+
+
+""" 
 Everytime you add a class( i.e. Table ), 
 run "flask db migrate -m "{your_message_here}"
 and "flask db upgrade" 
 to generate a new database migration
-'''
+"""
+
+
+# Leaderboard Table Creation
+class LeaderBoard(db.Model):
+    puzzle_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    word = db.Column(db.String(128))
+    username = db.Column(db.String(64))
+    time_spent = db.Column(db.Integer)
+    score = db.Column(db.Integer)
+
+    def __repr__(self):
+        return f"<LeaderBoard {self.word} - {self.username}>"
